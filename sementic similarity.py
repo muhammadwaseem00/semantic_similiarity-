8439f1d535f8d670844fd147a684619849b0c60f
Muhmmad Waseem <m.wasi17@gmail.com>
2023-02-16 00:52:44 +0500

    just added a comment

---
+++ sementic similarity.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+get_ipython().system('pip install sentence_transformers==2.2.2')
+
+
+# In[6]:
+
+
+# requirement -> sentence_transformers==2.2.2
+# reference
+# https://enjoymachinelearning.com/blog/finding-semantic-similarity-between-sentences-in-python/
+
+from sentence_transformers import SentenceTransformer, util
+import time
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Two lists of sentences
+sentences1 = [
+    "I want to know about financial advice about car loan.",
+]
+
+sentences2 = [
+    "How to get car loan financial advice",
+]
+
+t1 = time.time()
+# Compute embedding 
+embeddings1 = model.encode(sentences1, convert_to_tensor=True)
+embeddings2 = model.encode(sentences2, convert_to_tensor=True)
+
+# cosine-similarities
+cosine_scores = util.cos_sim(embeddings1, embeddings2)
+pt_cosine = util.pytorch_cos_sim(embeddings1, embeddings2)
+t2 = time.time()
+
+print(f"contextual similarity: {cosine_scores}, with time {t2 - t1} seconds")
+
+
+# In[9]:
+
+
+# requirement -> sentence_transformers==2.2.2
+# reference
+# https://enjoymachinelearning.com/blog/finding-semantic-similarity-between-sentences-in-python/
+
+from sentence_transformers import SentenceTransformer, util
+import time
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Two lists of sentences
+sentences1 = [
+    "I want to know about financial advice about car loan.",
+]
+
+sentences2 = [
+    "How to get car loan financial advice",
+]
+
+t1 = time.time()
+# Compute embedding 
+embeddings1 = model.encode(sentences1, convert_to_tensor=True)
+embeddings2 = model.encode(sentences2, convert_to_tensor=True)
+
+# cosine-similarities
+cosine_scores = util.cos_sim(embeddings1, embeddings2)
+cosine_scores = round(float(cosine_scores.numpy()[0][0]), 3)
+# pt_cosine = util.pytorch_cos_sim(embeddings1, embeddings2)
+t2 = time.time()
+
+print(f"contextual similarity: {cosine_scores}, with time {t2 - t1} seconds")
+
+
+# In[ ]:
+
+
+
+
